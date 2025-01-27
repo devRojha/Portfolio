6a01d58a4e7c2fdcf2c1357efae6bd198214d4ec
Devraj Kumar <113443021+devRojha@users.noreply.github.com>
2025-01-28 01:12:12 +0530

    adding more projects

--- frontend/components/Works.tsx
+++ frontend/components/Works.tsx
@@ -14,17 +14,29 @@ export default function Works(){
             <div className="mx-4">
                 <div className="font-bold text-pretty text-3xl mb-8">Works</div>
                 <div className="flex flex-wrap gap-4 justify-center">
-                    <a href="https://code.alpha-dev.tech/" className=" rounded-lg border flex flex-col" data-aos="fade-right">
+                    <a href="https://code.alpha-dev.tech/" className=" rounded-lg border flex flex-col" data-aos="fade-right" target="blank">
                         <ProjectIMG img={"CA"}  len={8}/>
                         <div className="text-center text-blue-500 font-bold py-2">Code.Alpha</div>
                     </a>
-                    <a href="https://elective.vercel.app/" className=" rounded-lg border flex flex-col" data-aos="fade-up">
+                    <a href="https://elective.vercel.app/" className=" rounded-lg border flex flex-col" data-aos="fade-down" target="blank">
                         <ProjectIMG img={"Elective"}  len={11}/>
                         <div className="text-center text-blue-500 font-bold py-2">Elective</div>
                     </a>
-                    <a href="https://iscesti2025.vercel.app/" className=" rounded-lg border flex flex-col" data-aos="fade-left" >
+                    <a href="https://www.luminaerospace.tech/" className=" rounded-lg border flex flex-col" data-aos="fade-left" target="blank">
+                        <ProjectIMG img={"Lumin"}  len={9}/>
+                        <div className="text-center text-blue-500 font-bold py-2">Lumin Aerospace</div>
+                    </a>
+                    <a href="https://iscesti2025.vercel.app/" className=" rounded-lg border flex flex-col" data-aos="fade-right" target="blank">
                         <ProjectIMG img={"ISCESTI"}  len={4}/>
                         <div className="text-center text-blue-500 font-bold py-2">ISCESTI2025</div>
+                    </a>
+                    <a href="https://www.nitp.ac.in/Department/" className=" rounded-lg border flex flex-col" data-aos="fade-up" target="blank">
+                        <ProjectIMG img={"NITP"}  len={7}/>
+                        <div className="text-center text-blue-500 font-bold py-2">NIT Patna All Department Section</div>
+                    </a>
+                    <a href="https://rollpark06.netlify.app/" className=" rounded-lg border flex flex-col" data-aos="fade-left" target="blank">
+                        <ProjectIMG img={"Rollpark"}  len={7}/>
+                        <div className="text-center text-blue-500 font-bold py-2">Rollpark</div>
                     </a>
                 </div>
             </div>
