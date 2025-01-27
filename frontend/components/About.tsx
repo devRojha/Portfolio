c00696792fff851d5e534f0a4ef0a124445c895e
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2025-01-27 15:53:18 +0530

    responsive hero and about

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from "react"
 import SkillAnimate from "./SkillAnimate"
 
 const SkillList = [
@@ -72,21 +73,24 @@ const SkillList = [
 
 
 export default function About(){
+    const [more , setMore] = useState<boolean>(false);
     return(
         <div id="About" className="mb-10">
             <div className="mx-4">
                 <div className="font-bold text-pretty text-3xl mb-8">ABOUT</div>
-                <div className="grid grid-cols-2 max-sm:grid-cols-1">
-                    <div className="pt-[300px] mr-3">
-                         <div className=" rounded-lg p-8">
+                <div className="grid grid-cols-2 max-md:grid-cols-1">
+                    <div className="pt-[300px] max-md:pt-8 max-md:mb-8">
+                         <div className="max-md:border-b rounded-lg p-4">
                             <div className="text-center font-bold text-2xl mb-4 text-yellow-500">Who Am I?</div>
-                            <div className="flex text-justify font-serif">
-                                As a second-year electrical engineering student, I am actively honing my skills in Software Development forming a robust foundation in the MERN stack. I&apos;ve demonstrated my problem-solving prowess by successfully navigating through over 300 complex questions on platforms like LeetCode, GeeksforGeeks, and CodeChef, achieving notable ratings. In the realm of robotics, I hold a key role as a technical coordinator in my university&apos;s robotics club, contributing significantly to various projects and showcasing practical problem-solving abilities alongside effective leadership skills. Beyond academics, I am a national-level badminton player, embodying qualities of discipline, commitment, and the ability to perform under pressure. Recently selected for an international conference on sports engineering, I actively contribute to a team project, reflecting my dedication to staying at the forefront of technological advancements. This integration of technical proficiency, leadership experiences, and athletic achievements defines my well-rounded approach, aligning with my passion for continuous learning and meaningful contributions to diverse environments.
+                            <div className="flex text-justify font-serif overflow-auto">
+                                <div className="h-[400px]">
+                                    As a second-year electrical engineering student, I am actively honing my skills in Software Development forming a robust foundation in the MERN stack. I&apos;ve demonstrated my problem-solving prowess by successfully navigating through over 300 complex questions on platforms like LeetCode, GeeksforGeeks, and CodeChef, achieving notable ratings. In the realm of robotics, I hold a key role as a technical coordinator in my university&apos;s robotics club, contributing significantly to various projects and showcasing practical problem-solving abilities alongside effective leadership skills. Beyond academics, I am a national-level badminton player, embodying qualities of discipline, commitment, and the ability to perform under pressure. Recently selected for an international conference on sports engineering, I actively contribute to a team project, reflecting my dedication to staying at the forefront of technological advancements. This integration of technical proficiency, leadership experiences, and athletic achievements defines my well-rounded approach, aligning with my passion for continuous learning and meaningful contributions to diverse environments.
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div>
-                        <div className="border-l rounded-lg p-8">
+                        <div className="border-l max-md:border-b max-md:border-l-0 rounded-lg p-4">
                             <div className="text-center font-bold text-2xl mb-4 text-yellow-500">Technical Skills</div>
                             <div className="space-y-2 font-serif">
                                 {SkillList.map((list)=>
