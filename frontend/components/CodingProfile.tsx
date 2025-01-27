0c8e7a583c9beb0957bc92b6d2c68c2cd18ea32e
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2025-01-28 01:20:11 +0530

    adding github profile

--- frontend/components/CodingProfile.tsx
+++ frontend/components/CodingProfile.tsx
@@ -15,9 +15,10 @@ export default function CodingProfile(){
             <div className="" data-aos="fade-up">
                 <div className="flex justify-center space-x-4 max-sm:space-x-2  py-4">
                     <a href="https://codeforces.com/profile/devraj_4" target="blank" data-aos="fade-right"><Image className=" w-[55px] px-2  " src={"/Codeforces.png"} width={1000} height={1000} alt="Loading"/></a>
-                    <a href="https://www.codechef.com/users/dev_r04" target="blank" data-aos="fade-right"><Image className=" w-[50px]  " src={"/codechef.png"} width={1000} height={1000} alt="Loading"/></a>
-                    <a href="https://leetcode.com/u/devraj_04/" target="blank" data-aos="fade"><Image className=" w-[60px] px-2 " src={"/Leetcode.png"} width={1000} height={1000} alt="Loading"/></a>
+                    <a href="https://www.codechef.com/users/dev_r04" target="blank" data-aos="fade-up"><Image className=" w-[50px]  " src={"/codechef.png"} width={1000} height={1000} alt="Loading"/></a>
+                    <a href="https://leetcode.com/u/devraj_04/" target="blank" data-aos="fade-right"><Image className=" w-[60px] px-2 " src={"/Leetcode.png"} width={1000} height={1000} alt="Loading"/></a>
                     <a href="https://www.geeksforgeeks.org/user/devraj_04/" target="blank" data-aos="fade-left"><Image className=" w-[55px] px-2 " src={"/GFG.png"} width={1000} height={1000} alt="Loading"/></a>
+                    <a href="https://github.com/devRojha/" target="blank" data-aos="fade-up"><Image className=" w-[55px] px-2 " src={"/Github.png"} width={1000} height={1000} alt="Loading"/></a>
                     <a href="https://www.youtube.com/@DuckScript" target="blank" data-aos="fade-left"><Image className=" w-[60px] px-2 " src={"/youtube.png"} width={1000} height={1000} alt="Loading"/></a>
                 </div>
             </div>
