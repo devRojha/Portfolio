cecae5423c34d4474fd058f937a66de2be0eefd4
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2025-01-27 18:57:39 +0530

    adding responsive project section

---
+++ frontend/components/ProjectIMG.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface PropsType {
+  img: string;
+  len: number;
+}
+
+export default function ProjectIMG(Props: PropsType) {
+  const [itr, setItr] = useState<number>(1);
+  const [fade, setFade] = useState<boolean>(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Trigger fade out
+      setFade(true);
+
+      // Wait for fade-out to complete, then update the image
+      setTimeout(() => {
+        if (itr === Props.len) {
+          setItr(1);
+        } else {
+          setItr(itr + 1);
+        }
+        // Trigger fade in
+        setFade(false);
+      }, 500); // Matches the CSS transition duration
+    }, 1500); // Time between image changes
+
+    return () => clearInterval(interval);
+  }, [itr, Props.len]);
+
+  return (
+    <div className="w-[400px] relative">
+      <Image
+        className={`h-[270px] rounded-lg transition-opacity duration-500 ${
+          fade ? "opacity-0" : "opacity-100"
+        }`}
+        src={`/${Props.img}${itr}.png`}
+        width={1000}
+        height={1000}
+        alt="Loading..."
+      />
+    </div>
+  );
+}
--- frontend/components/Projects.tsx
+++ frontend/components/Projects.tsx
@@ -1,18 +1,27 @@
 "use client"
 
+import ProjectIMG from "./ProjectIMG";
 
 
 export default function Projects(){
     return(
         <div id="Projects" className="mb-10">
             <div className="mx-4">
-                <div className="font-bold text-pretty text-3xl mb-8">Project</div>
-                <div>Code.Alpha</div>
-                <div>NITP Main Website</div>
-                <div>Elective</div>
-                <div>ISCESFET2025</div>
-                <div>Blog Medium</div>
-                <div>Pay TM</div>
+                <div className="font-bold text-pretty text-3xl mb-8">Projects</div>
+                <div className="flex flex-wrap gap-4 justify-center">
+                    <div className=" rounded-lg border flex flex-col">
+                        <ProjectIMG img={"CA"}  len={8}/>
+                        <div className="text-center font-bold py-2">Code.Alpha</div>
+                    </div>
+                    <div className=" rounded-lg border flex flex-col">
+                        <ProjectIMG img={"Elective"}  len={11}/>
+                        <div className="text-center font-bold py-2">Elective</div>
+                    </div>
+                    <div className=" rounded-lg border flex flex-col">
+                        <ProjectIMG img={"ISCESTI"}  len={4}/>
+                        <div className="text-center font-bold py-2">ISCESTI2025</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
