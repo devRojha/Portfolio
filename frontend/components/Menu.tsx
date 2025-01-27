ef9e417bbdf60417d90d61e1cb7d2bd08310798e
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2025-01-27 11:50:53 +0530

    deployement error resolved

--- frontend/components/Menu.tsx
+++ frontend/components/Menu.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 export default function Appbar() {
     return (
       <div className="fixed w-full z-30  bg-zinc-800 h-[70px] borde border-red-700 flex flex-col justify-center">
@@ -6,11 +6,11 @@ export default function Appbar() {
             <div className="font-bold text-5xl max-md:text-3xl"><span className="text-yellow-500">D</span>EVRAJ</div>
             <div className="flex flex-col justify-center">
                 <ul className="flex space-x-16 max-lg:space-x-10 font-bold">
-                    <li className="hover:border-b border-yellow-500"><a href="/">Home</a></li>
-                    <li className="hover:border-b border-yellow-500"><a href="/">About</a></li>
-                    <li className="hover:border-b border-yellow-500"><a href="/">Projects</a></li>
-                    <li className="hover:border-b border-yellow-500"><a href="/">More</a></li>
-                    <li className="hover:border-b border-yellow-500"><a href="/">Contacts</a></li>
+                    <li className="hover:border-b border-yellow-500"><a href="">Home</a></li>
+                    <li className="hover:border-b border-yellow-500"><a href="">About</a></li>
+                    <li className="hover:border-b border-yellow-500"><a href="">Projects</a></li>
+                    <li className="hover:border-b border-yellow-500"><a href="">More</a></li>
+                    <li className="hover:border-b border-yellow-500"><a href="">Contacts</a></li>
                 </ul>
             </div>
         </div>
